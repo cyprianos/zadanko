479bce07cef1f1c6a4753a756c01f58c9a7cc20f
cgepfert <cgepfert@gmail.com>
2015-02-08 13:09:23 +0100

    db

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send(cool());
+});
+
+app.get('/db', function(req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('select * from test_table', function(err, result) {
+      done();
+      if(err) {
+        console.error(err); response.send("Error" + err);
+      } else {
+        response.send(result.rows);
+      }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {
